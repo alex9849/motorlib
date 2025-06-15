99744a30d5bbbe4d48fe2eac087d9b031c9a55df
alex9849 <alexander@liggesmeyer.net>
2025-06-15 08:21:27 +0200

    Added XL9535Pin

---
+++ src/main/java/net/alex9849/motorlib/xl9535/XL9535Pin.java
@@ -0,0 +1,26 @@
+package net.alex9849.motorlib.xl9535;
+
+import net.alex9849.motorlib.pin.IOutputPin;
+import net.alex9849.motorlib.pin.PinState;
+
+public class XL9535Pin implements IOutputPin {
+    private final byte pinNr;
+    private final XL9535 xl9535;
+
+    public XL9535Pin(byte pinNr, XL9535 xl9535) {
+        this.pinNr = pinNr;
+        this.xl9535 = xl9535;
+    }
+
+    @Override
+    public synchronized void digitalWrite(PinState value) {
+        xl9535.writeRegister(pinNr, value == PinState.HIGH);
+    }
+
+    @Override
+    public synchronized boolean isHigh() {
+        return xl9535.readRegister(pinNr);
+    }
+
+
+}
